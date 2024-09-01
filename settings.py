88db6a2cb36e7d62407e032e5d44376c54ef2a74
Andrew gathu <gathuandrew7@gmail.com>
2024-09-01 22:27:57 +0300

    Add new django custom Models

---
+++ settings.py
@@ -0,0 +1,7 @@
+# ... (other settings remain the same)
+
+   ROOT_URLCONF = 'advanced_features_and_security.urls'
+
+   WSGI_APPLICATION = 'advanced_features_and_security.wsgi.application'
+
+   # ... (rest of the file remains the same)
